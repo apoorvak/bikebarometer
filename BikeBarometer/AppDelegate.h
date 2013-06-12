c74b220502d7daf3ce372d3c1268827dfaad3f45
Brian <minimac@Brians-Macbook-Pro.local>
2013-06-12 10:14:50 -0700

    Initial Commit

---
+++ BikeBarometer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BikeBarometer
+//
+//  Created by Brian on 6/12/13.
+//  Copyright (c) 2013 Cascade Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BikeBarometer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BikeBarometer
+//
+//  Created by Brian on 6/12/13.
+//  Copyright (c) 2013 Cascade Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
